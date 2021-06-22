cbff393fe284bdddd922f2c96be54d69096d2b71
DevExpressExampleBot <fakeEmail>
2021-06-22 14:55:07 +0300

    Source auto update [skip ci]

--- VB/DataFederationExample/Form1.vb
+++ VB/DataFederationExample/Form1.vb
@@ -1,11 +1,12 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.DashboardCommon
-Imports DevExpress.DataAccess.ConnectionParameters
 Imports DevExpress.DataAccess.DataFederation
+Imports DevExpress.XtraEditors
+Imports Microsoft.VisualBasic
+Imports DevExpress.DataAccess.ConnectionParameters
 Imports DevExpress.DataAccess.Excel
 Imports DevExpress.DataAccess.Json
 Imports DevExpress.DataAccess.Sql
-Imports DevExpress.XtraEditors
+
 Imports System
 
 Namespace DataFederationExample
